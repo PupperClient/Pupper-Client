8f06eee97920bfe9d368147f80706707b836baa0
oneachina <ruiruixi@hotmail.com>
2025-12-06 12:06:26 +0800

    Change the client name on the HomePage

--- src/main/java/cn/pupperclient/gui/modmenu/pages/HomePage.java
+++ src/main/java/cn/pupperclient/gui/modmenu/pages/HomePage.java
@@ -42,7 +42,7 @@ public class HomePage extends Page {
             palette.getSurfaceContainer());
 
         Skia.drawText("Welcome to", x + 50, y + 80, palette.getOnSurfaceVariant(), Fonts.getMedium(24));
-        Skia.drawText("PupperClient-CN " + PupperClient.getInstance().getVersion(), x + 50, y + 110, palette.getOnSurface(), Fonts.getRegular(36));
+        Skia.drawText("Pupper Client " + PupperClient.getInstance().getVersion(), x + 50, y + 110, palette.getOnSurface(), Fonts.getRegular(36));
 
         Skia.drawText(currentTime, x + width - 325, y + 80, palette.getOnSurfaceVariant(),
             Fonts.getMedium(24));
