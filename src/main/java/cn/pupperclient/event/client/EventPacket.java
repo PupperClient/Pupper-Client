390b6ac95aa6a32c37f0b3afc7f06b99b3eebf12
oneachina <ruiruixi@hotmail.com>
2025-12-06 18:51:17 +0800

    clean event

--- src/main/java/cn/pupperclient/event/client/EventPacket.java
+++
@@ -1,6 +0,0 @@
-package cn.pupperclient.event.client;
-
-import cn.pupperclient.event.Event;
-
-public class EventPacket extends Event {
-}
--- src/main/java/cn/pupperclient/management/mod/impl/fun/TotemTracker.java
+++ src/main/java/cn/pupperclient/management/mod/impl/fun/TotemTracker.java
@@ -1,14 +1,27 @@
 package cn.pupperclient.management.mod.impl.fun;
 
+import cn.pupperclient.event.EventBus;
+import cn.pupperclient.event.EventListener;
+import cn.pupperclient.event.client.ReceivePacketEvent;
+import cn.pupperclient.event.client.TotemEvent;
 import cn.pupperclient.management.mod.Mod;
 import cn.pupperclient.management.mod.ModCategory;
 import cn.pupperclient.skia.font.Icon;
+import cn.pupperclient.utils.ChatUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityStatuses;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.EntityStatusS2CPacket;
+
+import java.util.HashMap;
 
 public class TotemTracker extends Mod {
     private static TotemTracker instance;
+    public final HashMap<String, Integer> popContainer = new HashMap<>();
 
     public TotemTracker() {
         super("mod.totemtracker.name", "mod.totemtracker.description", Icon.SECURITY, ModCategory.FUN);
+        instance = this;
     }
 
     public static TotemTracker getInstance() {
@@ -23,5 +36,50 @@ public class TotemTracker extends Mod {
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    @EventListener
+    public void onTotem(TotemEvent event) {
+        PlayerEntity player = event.getPlayer();
+        int l_Count = 1;
+        if (popContainer.containsKey(player.getName().getString())) {
+            l_Count = popContainer.get(player.getName().getString());
+        }
+        if (l_Count == 1) {
+            if (player.equals(mc.player)) {
+                ChatUtils.addChatMessage(String.format("¡ìfYou(%s)¡ìr popped ¡ìf%d¡ìr totem.", player.getName().getString(), l_Count));
+            } else {
+                ChatUtils.addChatMessage(String.format("¡ìf%s¡ìr popped ¡ìf%d¡ìr totem.", player.getName().getString(), l_Count));
+            }
+        } else {
+            if (player.equals(mc.player)) {
+                ChatUtils.addChatMessage(String.format("¡ìfYou(%s)¡ìr popped ¡ìf%d¡ìr totem.", player.getName().getString(), l_Count));
+            } else {
+                ChatUtils.addChatMessage(String.format("¡ìf%s¡ìr has popped ¡ìf%d¡ìr totems.", player.getName().getString(), l_Count));
+            }
+        }
+    }
+
+    @EventListener(priority = 1001)
+    public void onPacketReceive(ReceivePacketEvent event) {
+        if (event.getPacket() instanceof EntityStatusS2CPacket packet) {
+            if (packet.getStatus() == EntityStatuses.USE_TOTEM_OF_UNDYING) {
+                Entity entity = packet.getEntity(mc.world);
+                if(entity instanceof PlayerEntity player) {
+                    onTotemPop(player);
+                }
+            }
+        }
+    }
+
+    public void onTotemPop(PlayerEntity player) {
+        int l_Count = 1;
+        if (popContainer.containsKey(player.getName().getString())) {
+            l_Count = popContainer.get(player.getName().getString());
+            popContainer.put(player.getName().getString(), ++l_Count);
+        } else {
+            popContainer.put(player.getName().getString(), l_Count);
+        }
+        EventBus.getInstance().post(new TotemEvent(player));
     }
 }
