1a03d8c98bc0dd0123e8c076ab541bad0989d6b1
oneachina <ruiruixi@hotmail.com>
2025-12-06 18:52:30 +0800

    Clean

--- src/main/java/cn/pupperclient/mixin/mixins/minecraft/entity/MixinLivingEntity.java
+++ src/main/java/cn/pupperclient/mixin/mixins/minecraft/entity/MixinLivingEntity.java
@@ -1,22 +1,13 @@
 package cn.pupperclient.mixin.mixins.minecraft.entity;
 
-import cn.pupperclient.event.EventBus;
-import cn.pupperclient.event.client.EventFallFlying;
-import cn.pupperclient.event.client.EventJump;
-import cn.pupperclient.event.client.EventRotationAnimation;
-import cn.pupperclient.event.client.EventUseTotem;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.DataTracker;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import cn.pupperclient.management.mod.impl.hud.JumpResetIndicatorMod;
@@ -26,7 +17,6 @@ import cn.pupperclient.mixin.interfaces.IMixinLivingEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.Hand;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity implements IMixinLivingEntity{
@@ -97,85 +87,4 @@ public abstract class MixinLivingEntity extends Entity implements IMixinLivingEn
 			this.preferredHand = hand;
 		}
 	}
-
-
-
-    @Redirect(
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/entity/LivingEntity;getYaw()F"
-        ),
-        method = {"turnHead"}
-    )
-    private float modifyHeadYaw(LivingEntity entity) {
-        float yaw = entity.getYaw();
-
-        if (entity instanceof PlayerEntity) {
-            EventRotationAnimation event = new EventRotationAnimation(entity.getYaw(), 0.0F, 0.0F, 0.0F);
-            EventBus.getInstance().post(event);
-            return event.getYaw();
-        } else {
-            return yaw;
-        }
-    }
-
-    @Redirect(
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/entity/LivingEntity;getYaw()F",
-            opcode = 182,
-            ordinal = 0
-        ),
-        method = {"jump"}
-    )
-    private float modifyJumpYaw(LivingEntity entity) {
-        float yaw = entity.getYaw();
-        if (entity instanceof PlayerEntity) {
-            EventJump event = new EventJump(yaw);
-            EventBus.getInstance().post(event);
-            return event.getYaw();
-        }
-        return yaw;
-    }
-
-//    @Redirect(
-//        method = "travelGliding",
-//        at = @At(
-//            value = "INVOKE",
-//            target = "Lnet/minecraft/entity/LivingEntity;calcGlidingVelocity(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"
-//        )
-//    )
-//    private Vec3d redirectCalcGlidingVelocity(LivingEntity instance, Vec3d oldVelocity) {
-//        EventFallFlying event = new EventFallFlying(instance.getPitch());
-//        EventBus.getInstance().post(event);
-//
-//        Vec3d originalVelocity = instance.calcGlidingVelocity(getVelocity());
-//
-//        if (event.getPitch() != instance.getPitch()) {
-//            return originalVelocity;
-//        }
-//
-//        return originalVelocity;
-//    }
-    @Redirect(
-        method = "travelGliding",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/entity/LivingEntity;calcGlidingVelocity(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"
-        )
-    )
-    private Vec3d redirectCalcGlidingVelocity(LivingEntity instance, Vec3d velocity) {
-        if (instance instanceof PlayerEntity) {
-            EventFallFlying event = new EventFallFlying(instance.getPitch());
-            EventBus.getInstance().post(event);
-            return instance.calcGlidingVelocity(velocity);
-        }
-        return instance.calcGlidingVelocity(velocity);
-    }
-    
-    @Inject(method = "tryUseDeathProtector", at = @At("HEAD"))
-    private void tryUseDeathProtector(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
-        EventUseTotem event = new EventUseTotem(source);
-        EventBus.getInstance().post(event);
-    }
 }
