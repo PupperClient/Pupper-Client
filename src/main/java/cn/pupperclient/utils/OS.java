f802f1d6429e158b57a5398b33810f5f21dc0150
oneachina <ruiruixi@hotmail.com>
2025-12-06 13:01:18 +0800

    Refactor OS using enums and streams

--- src/main/java/cn/pupperclient/utils/OS.java
+++ src/main/java/cn/pupperclient/utils/OS.java
@@ -1,18 +1,46 @@
 package cn.pupperclient.utils;
 
-public class OS {
+public enum OS {
+    WINDOWS("win"),
+    MACOS("mac"),
+    LINUX("nix", "nux", "aix"),
+    UNKNOWN();
 
-	private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
-	
-	public static boolean isWindows() {
-		return OS_NAME.contains("win");
-	}
-	
-	public static boolean isMacOS() {
-		return OS_NAME.contains("mac");
-	}
-	
-	public static boolean isLinux() {
-		return OS_NAME.contains("nix") || OS_NAME.contains("nux") || OS_NAME.contains("aix");
-	}
+    private final String[] identifiers;
+
+    OS(String... identifiers) {
+        this.identifiers = identifiers;
+    }
+
+    private static final OS CURRENT_OS = detectOS();
+
+    private static OS detectOS() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        return detectOS(osName);
+    }
+
+    private static OS detectOS(String osName) {
+        return java.util.Arrays.stream(values())
+            .filter(os -> os != UNKNOWN)
+            .filter(os -> java.util.Arrays.stream(os.identifiers)
+                .anyMatch(osName::contains))
+            .findFirst()
+            .orElse(UNKNOWN);
+    }
+
+    public static boolean isWindows() {
+        return CURRENT_OS == WINDOWS;
+    }
+
+    public static boolean isMacOS() {
+        return CURRENT_OS == MACOS;
+    }
+
+    public static boolean isLinux() {
+        return CURRENT_OS == LINUX;
+    }
+
+    public static OS getCurrent() {
+        return CURRENT_OS;
+    }
 }
