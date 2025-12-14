a56a227a9d20832026fc9365163f33cf7ad498d1
oneachina <ruiruixi@hotmail.com>
2025-12-14 12:42:56 +0800

    chore: delete user data

--- src/main/java/cn/pupperclient/utils/file/FileLocation.java
+++ src/main/java/cn/pupperclient/utils/file/FileLocation.java
@@ -12,7 +12,6 @@ public class FileLocation {
 	public static final File CONFIG_DIR = new File(MAIN_DIR, "config");
 	public static final File PROFILE_DIR = new File(MAIN_DIR, "profile");
     public static final File BACKGROUND_DIR = new File(MAIN_DIR, "background");
-    public static final File USER_DATA_DIR = new File(MAIN_DIR, "user.json");
     public static final File CAPES_DIR = new File(MAIN_DIR, "capes");
 
     public static void init() {
