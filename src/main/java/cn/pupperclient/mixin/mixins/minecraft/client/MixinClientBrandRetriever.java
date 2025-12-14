04bb1e41c8543ba52df6c810a4b7ab97ad4a9edf
oneachina <ruiruixi@hotmail.com>
2025-12-14 12:52:37 +0800

    feat: add Client Brand -> MixinClientBrandRetriever

--- src/main/java/cn/pupperclient/mixin/mixins/minecraft/client/MixinClientBrandRetriever.java
+++ src/main/java/cn/pupperclient/mixin/mixins/minecraft/client/MixinClientBrandRetriever.java
@@ -1,4 +1,20 @@
 package cn.pupperclient.mixin.mixins.minecraft.client;
 
+import cn.pupperclient.PupperClient;
+import net.minecraft.client.ClientBrandRetriever;
+import net.minecraft.obfuscate.DontObfuscate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(ClientBrandRetriever.class)
 public class MixinClientBrandRetriever {
+    /**
+     * @author oneachina
+     * @reason Set Pupper Brand
+     */
+    @DontObfuscate
+    @Overwrite
+    public static String getClientModName() {
+        return PupperClient.getInstance().getName() + "(" + PupperClient.getInstance().getVersion() + ")";
+    }
 }
