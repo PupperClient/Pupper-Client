faf430c812c7ef8d107f69414d0e349c5f3b39a8
oneachina <ruiruixi@hotmail.com>
2026-01-02 19:00:22 +0800

    chore: Bump Mod Version

--- src/main/java/cn/pupperclient/PupperClient.java
+++ src/main/java/cn/pupperclient/PupperClient.java
@@ -37,7 +37,7 @@ public class PupperClient implements IMinecraft {
     private static final String CONFIG_FILE_NAME = "pupper.ok";
     private static final String ICON_PATH = "assets/pupper/logo.png";
     private static final String CLIENT_NAME = "Pupper Client";
-    private static final String CLIENT_VERSION = "25.12.3";
+    private static final String CLIENT_VERSION = "26.1.1";
 
     private static final PupperClient INSTANCE = new PupperClient();
 
