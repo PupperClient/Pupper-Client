219e343eccd0a59b98f8c29e14d5efbab72d9cd4
oneachina <ruiruixi@hotmail.com>
2026-03-13 22:26:55 +0800

    rename logger

--- src/main/java/cn/pupperclient/PupperLogger.java
+++ src/main/java/cn/pupperclient/PupperLogger.java
@@ -5,22 +5,22 @@ import org.apache.logging.log4j.Logger;
 
 public class PupperLogger {
 
-	private static final Logger logger = LogManager.getLogger("PupperClient Client");
+	private static final Logger logger = LogManager.getLogger("Pupper Client");
 
 	public static void info(String prefix, String message) {
-		logger.info("[SC/INFO] [" + prefix + "] " + message);
+		logger.info("[Pupper/INFO] [" + prefix + "] " + message);
 	}
 
 	public static void warn(String prefix, String message) {
-		logger.warn("[SC/WARN] [" + prefix + "] " + message);
+		logger.warn("[Pupper/WARN] [" + prefix + "] " + message);
 	}
 
 	public static void error(String prefix, String message) {
-		logger.error("[SC/ERROR] [" + prefix + "] " + message);
+		logger.error("[Pupper/ERROR] [" + prefix + "] " + message);
 	}
 
 	public static void error(String prefix, String message, Exception e) {
-		logger.error("[SC/ERROR] [" + prefix + "] " + message, e);
+		logger.error("[Pupper/ERROR] [" + prefix + "] " + message, e);
 	}
 
 	public static Logger getLogger() {
