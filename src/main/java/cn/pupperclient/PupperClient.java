e84b3ae7e2054fdaf49c77af0ce0abd9dba638c0
oneachina <ruiruixi@hotmail.com>
2026-05-24 22:25:59 +0800

    fix: update CLIENT_VERSION to 9.0.0a

--- src/main/java/cn/pupperclient/PupperClient.java
+++ src/main/java/cn/pupperclient/PupperClient.java
@@ -29,7 +29,7 @@ public class PupperClient implements IMinecraft {
     private static final String CONFIG_FILE_NAME = "pupper.ok";
     private static final String ICON_PATH = "assets/pupper/logo.png";
     private static final String CLIENT_NAME = "Pupper Client";
-    private static final String CLIENT_VERSION = "mc26.1.2-pupper26.4.1";
+    private static final String CLIENT_VERSION = "mc26.1.2-9.0.0a";
     private static final String MOD_ID = "pupper";
 
     private static final PupperClient INSTANCE = new PupperClient();
