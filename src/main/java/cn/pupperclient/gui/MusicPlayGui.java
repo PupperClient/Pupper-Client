71b04ec779cb515cf144b812a1042e6b1f35c4df
oneachina <ruiruixi@hotmail.com>
2026-05-24 22:17:48 +0800

    refactor: optimize MusicPlayGui rendering and mouse handling

--- src/main/java/cn/pupperclient/gui/MusicPlayGui.java
+++ src/main/java/cn/pupperclient/gui/MusicPlayGui.java
@@ -2,15 +2,12 @@ package cn.pupperclient.gui;
 
 import cn.pupperclient.event.EventListener;
 import cn.pupperclient.event.skia.DrawSkiaEvent;
-import cn.pupperclient.event.skia.RenderSkiaEvent;
 import cn.pupperclient.gui.api.PupperGuiUtil;
 import cn.pupperclient.skia.Skia;
 import cn.pupperclient.skia.font.Fonts;
 import cn.pupperclient.skia.font.Icon;
 import cn.pupperclient.utils.minecraft.interfaces.IMinecraft;
 import cn.pupperclient.utils.mouse.MouseUtils;
-import io.github.humbleui.types.Rect;
-import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.network.chat.Component;
 import org.jspecify.annotations.NonNull;
@@ -20,23 +17,17 @@ import java.awt.*;
 public class MusicPlayGui extends PupperGuiUtil implements IMinecraft {
     private boolean isfullscreen = false;
 
+    private final int uiWidth = 1350;
+    private final int uiHeight = 900;
+
     public MusicPlayGui() {
         super(Component.literal("MusicPlay"));
     }
 
-    @Override
-    public void extractRenderState(final @NonNull GuiGraphicsExtractor graphics, final int mouseX, final int mouseY, final float a) {
-        checkWindow();
-        eventRegister();
-    }
-
     @EventListener
     public void draw(DrawSkiaEvent event) {
-        int uiWidth = 1350;
-        int uiHeight = 900;
-
-        int offsetX = (windowWidth - uiWidth) / 2;
-        int offsetY = (windowHeight - uiHeight) / 2;
+        var offsetX = (windowWidth - uiWidth) / 2;
+        var offsetY = (windowHeight - uiHeight) / 2;
 
         Skia.translate(offsetX, offsetY);
         Skia.drawRect(0, 0, 340, uiHeight, new Color(20, 20, 20, 240));
@@ -45,6 +36,7 @@ public class MusicPlayGui extends PupperGuiUtil implements IMinecraft {
         Skia.drawText("Minecraft Jagget MusicPlay", 20, 26, Color.WHITE, Fonts.getRegular(23));
 
         Skia.drawText(Icon.CLOSE, 1320, 20, Color.WHITE, Fonts.getIconFill(21));
+
         if (!isfullscreen) {
             Skia.drawText(Icon.FULLSCREEN, 1285, 19, Color.WHITE, Fonts.getIconFill(21));
         } else {
@@ -54,28 +46,27 @@ public class MusicPlayGui extends PupperGuiUtil implements IMinecraft {
     }
 
     @Override
-    public boolean mouseClicked(MouseButtonEvent click, boolean doubled) {
+    public boolean mouseClicked(@NonNull MouseButtonEvent click, boolean doubled) {
         var mouseX = click.x();
         var mouseY = click.y();
 
-        float uiWidth = 1350;
-        float uiHeight = 900;
+        var offsetX = (windowWidth - uiWidth) / 2;
+        var offsetY = (windowHeight - uiHeight) / 2;
 
-        float startX = (windowWidth - uiWidth) / 2;
-        float startY = (windowHeight - uiHeight) / 2;
+        var icon_CLOSE_rect = Skia.getTextBounds(Icon.CLOSE, Fonts.getIconFill(21));
+        var icon_FULLSCREEN_rect = Skia.getTextBounds(Icon.FULLSCREEN, Fonts.getIconFill(21));
+        var icon_MINIMIZE_rect = Skia.getTextBounds(Icon.MINIMIZE, Fonts.getIconFill(21));
 
-        Rect icon_CLOSE_rect =  Skia.getTextBounds(Icon.CLOSE, Fonts.getIconFill(21));
-        Rect icon_FULLSCREEN_rect =  Skia.getTextBounds(Icon.FULLSCREEN, Fonts.getIconFill(21));
-        Rect icon_FULLSCREEN_EXIT_rect =  Skia.getTextBounds(Icon.FULLSCREEN_EXIT, Fonts.getIconFill(21));
-        Rect icon_MINIMIZE_rect =  Skia.getTextBounds(Icon.MINIMIZE, Fonts.getIconFill(21));
-
-        if (MouseUtils.isInside(mouseX, mouseY, startX + 1320, startY + 20, icon_CLOSE_rect.getWidth(), icon_CLOSE_rect.getHeight()) ||
-            MouseUtils.isInside(mouseX, mouseY, startX + 1251, startY + 25, icon_MINIMIZE_rect.getWidth(), icon_MINIMIZE_rect.getHeight())
+        if (MouseUtils.isInside(mouseX, mouseY, offsetX + 1320, offsetY + 20, icon_CLOSE_rect.getWidth(), icon_CLOSE_rect.getHeight()) ||
+            MouseUtils.isInside(mouseX, mouseY, offsetX + 1251, offsetY + 25, icon_MINIMIZE_rect.getWidth(), icon_MINIMIZE_rect.getHeight())
         ) {
             client.setScreen(null);
-        } else if (MouseUtils.isInside(mouseX, mouseY, startX + 1285, startY + 19, icon_FULLSCREEN_rect.getWidth() + 1, icon_FULLSCREEN_rect.getHeight() + 1)) {
-            isfullscreen = !isfullscreen;
+            return true;
         }
-        return true;
+        if (MouseUtils.isInside(mouseX, mouseY, offsetX + 1285, offsetY + 19, icon_FULLSCREEN_rect.getWidth() + 1, icon_FULLSCREEN_rect.getHeight() + 1)) {
+            isfullscreen = !isfullscreen;
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/cn/pupperclient/gui/api/PupperGuiUtil.java
+++ src/main/java/cn/pupperclient/gui/api/PupperGuiUtil.java
@@ -5,10 +5,11 @@
 package cn.pupperclient.gui.api;
 
 import cn.pupperclient.event.EventBus;
-import cn.pupperclient.skia.Skia;
 import cn.pupperclient.utils.minecraft.interfaces.IMinecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import org.jspecify.annotations.NonNull;
 
 public abstract class PupperGuiUtil extends Screen implements IMinecraft {
     public int windowWidth = 0;
@@ -22,8 +23,8 @@ public abstract class PupperGuiUtil extends Screen implements IMinecraft {
 
     protected void checkWindow() {
         if (windowHeight == 0 || windowWidth == 0) {
+            this.windowWidth = client.getWindow().getWidth();
             this.windowHeight = client.getWindow().getHeight();
-            this.windowHeight = client.getWindow().getWidth();
         }
     }
 
@@ -62,5 +63,11 @@ public abstract class PupperGuiUtil extends Screen implements IMinecraft {
     @Override
     public boolean isPauseScreen() {
         return false;
+    }
+
+    @Override
+    public void extractRenderState(final @NonNull GuiGraphicsExtractor graphics, final int mouseX, final int mouseY, final float a) {
+        checkWindow();
+        eventRegister();
     }
 }
